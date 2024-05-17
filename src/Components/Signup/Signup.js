cb6b4255236d48387635080a46de5b400bfd4367
SREEKUTTAN N <sreekuttan1248@gmail.com>
2024-05-17 20:24:24 +0530

    inital commit

---
+++ src/Components/Signup/Signup.js
@@ -0,0 +1,98 @@
+import React, { useState,useContext} from 'react';
+
+import Logo from '../../olx-logo.png';
+import './Signup.css';
+import { FirebaseContext } from '../../Store/Context';
+import { useHistory } from 'react-router-dom';
+export default function Signup() {
+
+  const [name,setName]=useState('')
+  const [email,setEmail]=useState('')
+  const [phone,setPhone]=useState('')
+  const [password,setPassword]=useState('')
+  const {firebase}=useContext(FirebaseContext)
+  const history=useHistory()
+
+
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    console.log(firebase);
+    firebase.auth().createUserWithEmailAndPassword(email,password)
+    .then((result)=>{
+      result.user.updateProfile({displayName:name})
+      .then(()=>{
+        firebase.firestore().collection('users').add({
+          id:result.user.uid,
+          username:name,
+          phone:phone,
+          email:email,
+          password:password
+        }).then(()=>{
+          history.push('login')
+        })
+      })
+    })
+  }
+
+
+  return (
+    <div>
+      <div className="signupParentDiv">
+        <img width="200px" height="200px" src={Logo}></img>
+        <form className="signUpform" onSubmit={handleSubmit}>
+          <label htmlFor="fname">Username</label>
+          <br />
+          <input
+            className="input"
+            type="text"
+            value={name}
+            onChange={(e)=>setName(e.target.value)}
+            id="fname"
+            name="name"
+            
+          />
+          <br />
+          <label htmlFor="fname">Email</label>
+          <br />
+          <input
+            className="input"
+            type="email"
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
+            id="fname"
+            name="email"
+           
+          />
+          <br />
+          <label htmlFor="lname">Phone</label>
+          <br />
+          <input
+            className="input"
+            type="number"
+            value={phone}
+            onChange={(e)=>setPhone(e.target.value)}
+            id="lname"
+            name="phone"
+            
+          />
+          <br />
+          <label htmlFor="lname">Password</label>
+          <br />
+          <input
+            className="input"
+            type="password"
+            value={password}
+            onChange={(e)=>setPassword(e.target.value)}
+            id="lname"
+            name="password"
+            
+          />
+          <br />
+          <br />
+          <button>Signup</button>
+        </form>
+        <a>Login</a>
+      </div>
+    </div>
+  );
+}
---
+++ src/Firebase/config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+import 'firebase/auth'
+import 'firebase/firebase'
+import 'firebase/storage'
+const firebaseConfig = {
+    apiKey: "AIzaSyDFCkzqr5I5r8zU7GQsOZp1hTFt4o8FJ7U",
+    authDomain: "olx-clone-74d38.firebaseapp.com",
+    projectId: "olx-clone-74d38",
+    storageBucket: "olx-clone-74d38.appspot.com",
+    messagingSenderId: "251982766745",
+    appId: "1:251982766745:web:185dac27abb4d889b60e08",
+    measurementId: "G-2QR4T0YDXT"
+  };
+ export default firebase.initializeApp(firebaseConfig)
